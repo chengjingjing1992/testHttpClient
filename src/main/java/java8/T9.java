d870f26d12e4c163a6b6053d8b22ea833364d9ce
chengjingjing1992 <2629704685@qq.com>
2020-11-23 22:09:33 +0800

    提交 lambda 表达式

--- src/main/java/java8/T9.java
+++ src/main/java/java8/T9.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 
 import java.io.PrintStream;
 import java.util.Comparator;
+import java.util.function.BiPredicate;
 import java.util.function.Consumer;
 import java.util.function.Supplier;
 
@@ -16,12 +17,27 @@ import java.util.function.Supplier;
  * 1 对象::实例方法名
  * 2 类::静态方法名
  * 3 类::实例方法名
- * 注意 lamba 体中调用方法的参数列表与返回值类型，要与函数式接口中抽象方法的函数列表和返回值类型保持一致！
- *
+ * 注意 :
+ * 1)lamba 体中调用方法的参数列表与返回值类型，要与函数式接口中抽象方法的函数列表和返回值类型保持一致！
+ * 2) 若lamda参数列表中的第一参数是实例方法的调用者，而第二个参数是实例方法的参数时 可以使用 className::method
  */
 public class T9 {
     @Test
+    public void tess5(){
+        /**
+         * 类::实例方法
+         */
+        BiPredicate biPredicate=(x,y)->x.equals(y);
+        biPredicate.test("abc","abc");
+        BiPredicate<String,String> biPredicate2=String::equals;
+
+
+    }
+    @Test
     public void test1(){
+        /**
+         * 对象::实例方法
+         */
         System.out.println();
         Consumer consumer=(x)-> System.out.println(x);
         PrintStream printStream=System.out;
@@ -32,6 +48,9 @@ public class T9 {
 
     @Test
     public void test3(){
+        /**
+         * 对象::实例方法
+         */
         Person person=new Person(1,"小明",12);
         Supplier supplier=()->person.getAge();
 
@@ -46,10 +65,17 @@ public class T9 {
 
     @Test
     public void test4(){
+        /**
+         * 类::静态方法
+         */
         Comparator<Integer> comparator=(x,y)->Integer.compare(x,y);
         System.out.println(comparator.compare(6,5));
+        Comparator<Integer> comparator1=Integer::compare;
+        comparator1.compare(5,6);
+
 
     }
+
 
 
 
